7f6bdee4cf4bec3eb31f44800936364b7f989871
Ansh Gupta <ng607301@gmail.com>
2025-05-30 17:25:24 +0530

    backend files setup

---
+++ server/router/auth-router.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+const authcontroller= require("../controller/auth-controller")
+
+
+
+router.route("/").get(authcontroller.home)
+router.route("/register").get(authcontroller.register)
+router.route("/login").get(authcontroller.login)
+
+
+module.exports = router
