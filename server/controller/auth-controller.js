79d5312de173372f789ba087c56676a926f66e25
Ansh Gupta <ng607301@gmail.com>
2025-05-31 09:12:46 +0530

    frontend files setup

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,31 +1,31 @@
 //home page controller
 
-const home = async(req,res)=>{
-    try {
-       res.status(200).send("hi i am home page") 
-    } catch (error) {
-        console.log(error)
-    }
-}
+const home = async (req, res) => {
+  try {
+    res.status(200).send("hi i am home page");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // registration controller
 
-const register = async(req,res)=>{
-try {
-    res.status(200).send("register")
-} catch (error) {
-    console.log(error)
-}
-}
+const register = async (req, res) => {
+  try {
+    res.status(200).send("register");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // login controller
 
-const login = (req,res)=>{
-    try {
-        res.status(300).send("login")
-    } catch (error) {
-        console.log(error)
-    }
-}
+const login = (req, res) => {
+  try {
+    res.status(300).send("login");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = {home,register,login}
+module.exports = { home, register, login };
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,12 +1,9 @@
-const express = require("express")
-const router = express.Router()
-const authcontroller= require("../controller/auth-controller")
+const express = require("express");
+const router = express.Router();
+const authcontroller = require("../controller/auth-controller");
 
+router.route("/").get(authcontroller.home);
+router.route("/register").post(authcontroller.register);
+router.route("/login").get(authcontroller.login);
 
-
-router.route("/").get(authcontroller.home)
-router.route("/register").get(authcontroller.register)
-router.route("/login").get(authcontroller.login)
-
-
-module.exports = router
+module.exports = router;
